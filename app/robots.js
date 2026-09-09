ebdb28dfcea0e603c72343c9d695034fd5186f26
조문국 <120075237+graffityart@users.noreply.github.com>
2026-09-09 12:10:59 +0900

    Update robots host and sitemap to pintoss.co.kr

--- app/robots.js
+++ app/robots.js
@@ -1,11 +1,7 @@
 export default function robots(){
   return {
-    rules:[
-      {userAgent:'*',allow:'/',disallow:['/admin/','/api/']},
-      {userAgent:'Yeti',allow:'/',disallow:['/admin/','/api/']},
-      {userAgent:'Googlebot',allow:'/',disallow:['/admin/','/api/']}
-    ],
-    sitemap:'https://seoyo.kr/sitemap.xml',
-    host:'https://seoyo.kr'
+    rules:[{userAgent:'*',allow:'/',disallow:['/admin/','/api/']},{userAgent:'Yeti',allow:'/',disallow:['/admin/','/api/']},{userAgent:'Googlebot',allow:'/',disallow:['/admin/','/api/']}],
+    sitemap:'https://www.pintoss.co.kr/sitemap.xml',
+    host:'https://www.pintoss.co.kr'
   };
 }
